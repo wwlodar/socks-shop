0dd1a3e6020ff3abbfe20252d6064cf6c57c15fb
wwlodar <58707277+wwlodar@users.noreply.github.com>
2021-10-06 14:38:11 +0200

    fixes

--- socks_shop/socks_shop/settings.py
+++ socks_shop/socks_shop/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, "static/"),
+  os.path.join(BASE_DIR, 'staticfiles'),
 )
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = 'uploads/'
 
