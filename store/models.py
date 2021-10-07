86b3ef08780dd5fb81e882ecd047ff0393532e4f
wwlodar <58707277+wwlodar@users.noreply.github.com>
2021-10-07 19:08:24 +0200

    Update models.py

--- store/models.py
+++ store/models.py
@@ -25,7 +25,7 @@ class Product(models.Model):
   def save(self, *args, **kwargs):
     super().save(*args, **kwargs)
 
-    img = Image.open(self.image.path)
+    img = Image.open(self.image)
 
     if img.height > 300 or img.width > 300:
       output_size = (300, 300)
