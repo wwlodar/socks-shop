7f1b7bac0921d48d6647c1c7248420753361dd22
wwlodar <58707277+wwlodar@users.noreply.github.com>
2021-10-27 10:22:17 +0200

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sp1oy6n=(gek=@$#h2n)r7cb-=ueyo$onv+cy@io-h7*9y-8qn'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
@@ -90,7 +90,7 @@ DATABASES = {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'socks_shop',
     'USER': 'postgres',
-    'PASSWORD': 'Pass',
+    'PASSWORD': os.environ['POSTGRES_PASSWORD'],
     'HOST': 'localhost',
     'PORT': '',
   }
